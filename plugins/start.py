8805c88a621b2d1678a308d24ad64b43b9e68af4
Arpitgavla <164368538+Arpitgavla@users.noreply.github.com>
2024-07-29 21:53:01 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -21,7 +21,7 @@ from database.database import add_user, del_user, full_userbase, present_user
 
 # add time im seconds for waitingwaiting before delete 
 # 1 minutes = 60, 2 minutes = 60×2=120, 5 minutes = 60×5=300
-SECONDS = int(os.getenv("SECONDS", "1800"))
+SECONDS = int(os.getenv("SECONDS", "600"))
 
 
 
@@ -94,7 +94,7 @@ async def start_command(client: Client, message: Message):
                 
             except:
                 pass
-        k = await client.send_message(chat_id = message.from_user.id, text=f"<b>❗️ <u>IMPORTANT</u> ❗️</b>\n\nThis video / file will be deleted in 30 minutes (Due to copyright issues).\n\n📌 Please forward this video / file to somewhere else and start downloading there.")
+        k = await client.send_message(chat_id = message.from_user.id, text=f"<b>❗️ <u>IMPORTANT</u> ❗️</b>\n\nThis video / file will be deleted in 10 minutes (Due to copyright issues).\n\n📌 Please forward this video / file to somewhere else and start downloading there.")
         await asyncio.sleep(SECONDS)
         for i in ms:
             await client.delete_messages(message.from_user.id,int(i))
